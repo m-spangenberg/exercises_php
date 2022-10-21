21e86494480491e9ab0a688d476adb804607fdfa
m-spangenberg <mspangenberg.dev@gmail.com>
2022-10-21 22:46:01 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Docker: Local PHP Development Server
+
+```bash
+mkdir data
+# place your php files in data/
+sudo docker compose -f docker-compose.yml up -d
+```
+
+## Documentation
+
+https://www.php.net/manual/en/
---
+++ ex_syntax/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Syntax</title>
+</head>
+<body>
+
+    <?php
+
+        $declaredVariable;  // declared by not assigned
+
+        CONST PI = 3.14;  // constant declaration and assignment (capitalized, no $)
+    
+        if (1 == 1) {
+            echo "<p>This is some text</p>"
+        }
+
+        $myName = "username";  // string
+        $myAge  100;  // integer
+        $canCode = true;  // boolean
+
+        $num1 = 10
+        $num2 = 20
+        $num1 += $num2
+        echo $num1
+
+        // = assignment, == equality, === strict equality (like in JavaScript)
+    ?>
+
+</body>
+</html>
