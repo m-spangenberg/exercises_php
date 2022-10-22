5d7db958b372eaddb79173558c46734f8a60a948
m-spangenberg <mspangenberg.dev@gmail.com>
2022-10-22 10:33:41 +0200

    chore: exercises

---
+++ ex_linking/linked.php
@@ -0,0 +1,3 @@
+<?php
+
+$linkedVariable = "IT VERKS!";
---
+++ ex_templating/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Templating PHP</title>
+</head>
+<body>
+
+    <?php
+        include_once 'header.php';
+    ?>
+
+    <section>
+        <h1>Some title</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, numquam tempora dolorem ut omnis, eligendi voluptatem ducimus nihil impedit ex minus ipsam non earum veniam eum repellat! Veniam, ea ipsam?</p>
+    </section>
+
+    <?php
+        include_once 'footer.php';
+    ?>
+    
+</body>
+</html>
